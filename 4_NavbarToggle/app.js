a5c00aa5eac6248e538914b9bbc2900fdb0bb1b8
Gawain <a0900492616@gmail.com>
2022-12-28 15:49:23 +0800

    1228 second

--- 4_NavbarToggle/app.js
+++ 4_NavbarToggle/app.js
@@ -3,3 +3,18 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    // links.classList.remove('links')
+    // links.classList.add('show-links')
+    if (links.classList.contains('show-links')){
+        links.classList.remove('show-links')
+    } else {
+        links.classList.add("show-links");
+    }
+    // console.log(links.classList)
+
+});
